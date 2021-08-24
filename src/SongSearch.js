c37e33dbc5d7c1095f4f4a4765c752205e2b5a67
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2021-08-24 20:30:43 +0200

    .

--- src/SongSearch.js
+++ src/SongSearch.js
@@ -72,27 +72,155 @@ function useSongSearch(searchTerm) {
     // {
     //   title: "Twist In My Sobriety",
     //   chords: ["Em", "Am7", "B"],
-    //   lines: [["Em"], ["Am7", "B"]] //, ["C", "A", "C"]]
+    //   lines: [
+    //     ["Verse"],
+    //     ["Em", "Am7", "B", "Em"],
+    //     ["Em", "Am7", "B", "Em"],
+    //     ["B", "Em"],
+    //     ["Chorus"],
+    //     ["B", "Em"],
+    //     ["B", "Em"],
+    //     ["Am7", "Em", "B", "Em"],
+    //     ["B", "Em", "B", "(Em)"],
+    //     ["Break"],
+    //     ["to do..."],
+    //     ["to do..."],
+    //     ["to do..."]
+    //   ],
+    //   links: [
+    //     "https://tabs.ultimate-guitar.com/tab/tanita-tikaram/twist-in-my-sobriety-chords-1394609"
+    //   ]
     // },
     {
-      title: "I Love You",
-      chords: ["A", "B", "C"],
-      lines: [
-        ["A", "B", "C"],
-        ["A", "B", "C"]
+      title: "That's Allright Mama",
+      chords: ["C", "C7", "F", "G7"],
+      lines: [["C"], ["C"], ["C", "C7"], ["C7", "F"], ["F", "G7", "C"]],
+      links: [
+        "https://www.ukulele-tabs.com/uke-songs/elvis-presley/thats-alright-mama-uke-tab-66753.html",
+        "https://www.youtube.com/watch?v=u4KpXlanzDA"
       ],
-      links: ["https://bouwe.io", "https://youtube.com"],
       kind: "song"
     },
     {
-      title: "My Song",
-      chords: ["A", "D", "E"],
+      title: "Down In The River To Pray",
+      chords: ["G", "C", "D7"],
       lines: [
-        ["A", "D", "E"],
-        ["E", "D", "E"]
+        ["Chorus"],
+        ["G"],
+        ["C", "G"],
+        ["G"],
+        ["D7", "C", "G"],
+        ["Verse"],
+        ["D7", "C", "G"],
+        ["G", "C", "G"],
+        ["D7", "C", "G"],
+        ["D7", "C", "G"]
       ],
-      links: [],
-      kind: "chords"
+      links: [
+        "https://www.ukulele-tabs.com/uke-songs/folk-songs/down-in-the-river-to-pray-uke-tab-67392.html"
+      ],
+      kind: "song"
+    },
+    {
+      title: "Stand By Me",
+      chords: ["C", "Am", "F", "G"],
+      lines: [
+        ["Intro"],
+        ["C", "Am", "F", "G", "C"],
+        ["Verse"],
+        ["C", "Am"],
+        ["F", "G", "C"],
+        ["C", "Am"],
+        ["F", "G", "C"],
+        ["Chorus"],
+        ["C"],
+        ["Am"],
+        ["F", "G", "C"],
+        ["Instrumental"],
+        ["C", "Am", "F", "G", "C"]
+      ],
+      links: [
+        "https://www.ukulele-tabs.com/uke-songs/ben-e-king/stand-by-me-uke-tab-15389.html"
+      ],
+      kind: "song"
+    },
+    {
+      title: "Hey Good Lookin'",
+      chords: ["C", "D", "F", "G"],
+      lines: [["C"], ["C"], ["D", "G", "C"], ["C"], ["C"], ["D", "G", "C"]],
+      links: ["https://ukutabs.com/h/hank-williams/hey-good-lookin/"],
+      kind: "song"
+    },
+    {
+      title: "You Are My Sunshine",
+      chords: ["C", "C7", "G7", "F", "G"],
+      lines: [
+        ["C", "C7"],
+        ["C7", "G7", "C", "F", "C", "C7"],
+        ["C7", "G7", "C", "F", "C", "C7"],
+        ["C", "G7", "C"]
+      ],
+      links: [
+        "https://www.ukulele-tabs.com/uke-songs/folk-songs/you-are-my-sunshine-uke-tab-44689.html"
+      ],
+      kind: "song"
+    },
+    {
+      title: "It's Now Or Never",
+      chords: ["C", "Dm", "G", "G7", "Fm"],
+      lines: [
+        ["Intro"],
+        ["C", "G7", "C"],
+        ["Chorus"],
+        ["C", "Dm"],
+        ["G7", "C"],
+        ["Fm", "C"],
+        ["G7", "C"],
+        ["Verse"],
+        ["C", "Dm"],
+        ["G7", "C"],
+        ["C", "Dm"],
+        ["C", "G7", "C"]
+      ],
+      links: ["https://www.youtube.com/watch?v=h_XsMQZlzrQ"],
+      kind: "song"
+    },
+    {
+      title: "Cups (When I'm Gone)",
+      chords: ["C", "F", "Am", "G"],
+      lines: [["to do..."], ["to do..."], ["to do..."], ["to do..."]],
+      links: ["https://www.youtube.com/watch?v=JGI1LWfFIZQ"],
+      kind: "song"
+    },
+
+    // {
+    //   title: "Hot Potato",
+    //   chords: ["C", "A", "G#", "G", "Dm"],
+    //   lines: [
+    //     ["Chorus"],
+    //     ["C"],
+    //     ["C"],
+    //     ["C"],
+    //     ["A"],
+    //     ["G#", "G", "C"],
+    //     ["Whiggy, Whiggy"],
+    //     ["Dm"],
+    //     ["Dm"],
+    //     ["C"]
+    //   ],
+    //   links: [
+    //     "https://tabs.ultimate-guitar.com/tab/the-wiggles/hot-potato-ukulele-1380803"
+    //   ],
+    //   kind: "song"
+    // },
+    {
+      title: "I Saw The Light",
+      chords: ["G", "C", "D"],
+      lines: [["G"], ["C", "G"], ["G"], ["G", "D", "G"]],
+      links: [
+        "https://www.ukulele-tabs.com/uke-songs/hank-williams/i-saw-the-light-uke-tab-47708.html"
+      ],
+      kind: "song"
     }
   ];
 
